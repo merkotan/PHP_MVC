21757e18ece2708c0e317c954aae4895d07af225
Gala Merkotan <merkatan@ukr.net>
2018-09-02 19:42:52 +0300

    load models

--- libs/Bootstrap.php
+++ libs/Bootstrap.php
@@ -7,11 +7,6 @@ use Weather\Controllers\Feedback;
 use Weather\Controllers\Feeds;
 use Weather\Controllers\Login;
 
-use Weather\Models\WeatherModel;
-use Weather\Models\LoginModel;
-use Weather\Models\FeedsModel;
-use Weather\Models\FeedbackModel;
-
 class Bootstrap
 {
 	function __construct()	
@@ -21,20 +16,15 @@ class Bootstrap
 		$url = explode('/',$url);
 		if (empty($url[0])) {
 			$controller = new Weather;
-			$model = new WeatherModel;
-			//$controller->loadModel('Weather');
+
 		} else {
 			$path = 'controllers/'.$url[0].'.php';
 			$pathm = 'models/'.$url[0].'model.php';
 				if ((file_exists($path))&&(file_exists($pathm))) {
 					$u=CONTROLLER_NM.$url[0];
 					$controller = new $u;
-					$u=MODEL_NM.ucfirst($url[0]).'Model';
-					$model = new $u;
-				//	$controller->loadModel($url[0]);
 				} else {
 					$controller = new Weather();
-					$model = new WeatherModel;
 				}	
 		}
 		if (!empty($url[2])) {
